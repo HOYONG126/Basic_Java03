ed0f6bdee1cb4246b5d805f6b410aa7567e21cfb
itwill <itwill@DESKTOP-AHDHN92>
2023-04-17 17:24:44 +0900

    4

--- Java_3/src/xyz/itwill/lang/thread/AccountUser.java
+++ Java_3/src/xyz/itwill/lang/thread/AccountUser.java
@@ -1,7 +1,7 @@
 package xyz.itwill.lang.thread;
 
 //은행계좌 사용자정보(은행계좌, 사용자명)를 저장하기 위한 클래스
-public class AccountUser {
+public class AccountUser { 
 	private Account account; //은행계좌정보-포함관계
 	private String username; //사용자명
 	public AccountUser() {
@@ -23,4 +23,6 @@ public class AccountUser {
 	public void setUsername(String username) {
 		this.username = username;
 	}
+	
+	
 }
