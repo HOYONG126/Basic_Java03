526a14925264c429afbba1ca972bd23b05070438
itwill <itwill@DESKTOP-AHDHN92>
2023-04-17 13:17:34 +0900

    4

--- Java_3/src/xyz/itwill/lang/thread/SingleThreadApp.java
+++ Java_3/src/xyz/itwill/lang/thread/SingleThreadApp.java
@@ -2,6 +2,7 @@ package xyz.itwill.lang.thread;
 //스레드(Thread) : 프로그램에서 명령을 실행하기 위한 최소의 작업단위-프로그램의 흐름
 //프로세스(process):
 
+//단일 스레드 프로그램
 public class SingleThreadApp {
 	public static void main(String[] args) {
 
