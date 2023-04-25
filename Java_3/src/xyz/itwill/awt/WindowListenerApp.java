c011223d1b914676692ffe1f5dd5ffaf33e36638
itwill <itwill@DESKTOP-AHDHN92>
2023-04-25 09:20:13 +0900

    4

--- Java_3/src/xyz/itwill/awt/WindowListenerApp.java
+++ Java_3/src/xyz/itwill/awt/WindowListenerApp.java
@@ -1,12 +1,11 @@
 package xyz.itwill.awt;
 
-import java.awt.Frame;
 
-public class WindowListenerApp extends Frame {
+public class WindowListenerApp{
 	
-	private static final long serialVersionUID = 1L;
 
 	public static void main(String[] args) {
+		
 	}
 	
 }
